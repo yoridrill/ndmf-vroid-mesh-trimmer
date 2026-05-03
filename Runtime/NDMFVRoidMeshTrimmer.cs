2213d13f3695f65e05a139d82cdf28e691ed6f2f
midori yoridori <yoridrill@gmail.com>
2026-05-03 16:29:44 +0900

    Add local-direction crossing order API and boundary-point counting

--- Runtime/NDMFVRoidMeshTrimmer.cs
+++ Runtime/NDMFVRoidMeshTrimmer.cs
@@ -58,6 +58,8 @@ public class NDMFVRoidMeshTrimmer : MonoBehaviour, IEditorOnly
     [Range(0f, 1f)] public float minIntersectionT = 0.02f;
 
     [Range(0f, 1f)] public float maxIntersectionT = 0.98f;
+    [Min(1)] public int edgeSampleSegments = 4;
+    [Min(1)] public int maxCrossingsPerEdge = 2;
 
     [Min(0f)] public float minTriangleUvArea = 0.0000001f;
     [Min(0f)] public float minTriangleWorldArea = 0.0000001f;
